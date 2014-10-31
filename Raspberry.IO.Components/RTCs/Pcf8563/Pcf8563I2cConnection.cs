14ca52b269b255748909496dcbe76ddf02fc80b0
gamondue <gamondue@gmail.com>
2014-10-31 02:40:01 +0100

    Component & Test for Real Time Clock PCF-8563. Still not working.

--- Raspberry.IO.Components/RTCs/Pcf8563/Pcf8563I2cConnection.cs
+++ Raspberry.IO.Components/RTCs/Pcf8563/Pcf8563I2cConnection.cs
@@ -6,7 +6,7 @@ using Raspberry.IO.InterIntegratedCircuit;
 
 #endregion
 
-namespace Raspberry.IO.Components.Expanders.Pcf8563
+namespace Raspberry.IO.Components.Rtcs.Pcf8563
 {
     /// <summary>
     /// Represents a I2C connection to a Pcf8563 Real Time Clock.
@@ -17,9 +17,6 @@ namespace Raspberry.IO.Components.Expanders.Pcf8563
         #region Fields
 
         private readonly I2cDeviceConnection connection;
-
-        private byte inputPins;
-        private byte currentStatus;
 
         #endregion
 
@@ -32,90 +29,87 @@ namespace Raspberry.IO.Components.Expanders.Pcf8563
         public Pcf8563I2cConnection(I2cDeviceConnection connection)
         {
             this.connection = connection;
-            connection.WriteByte(0);
         }
 
         #endregion
 
         #region Methods
         
-        /// <summary>
-        /// Sets the pin status.
-        /// </summary>
-        /// <param name="pin">The pin.</param>
-        /// <param name="enabled">if set to <c>true</c>, specified pin is enabled.</param>
-        public void SetPinStatus(Pcf8563Pin pin, bool enabled)
+
+        public DateTime ReadDateTime()
         {
-            var bit = GetPinBit(pin);
-            if ((inputPins & bit) != 0x00)
-                throw new NotSupportedException(string.Format(CultureInfo.InvariantCulture, "Cannot set value of input pin {0}", pin));
-
-            var status = currentStatus;
-            var newStatus = (byte)(enabled
-                ? status | bit
-                : status & ~bit);
-
-            connection.Write(newStatus);
-            currentStatus = newStatus;
+            byte[] rd = connection.Read(4); 
+    //        returndata = self._bus.read_byte_data(self._addr, data)
+    //#print "addr = 0x%x data = 0x%x %i returndata = 0x%x %i " % (self._addr, data, data, returndata, _bcd_to_int(returndata))
+    //    return returndata 
+            return DateTime.Now; 
         }
 
-        /// <summary>
-        /// Gets the pin status.
-        /// </summary>
-        /// <param name="pin">The pin.</param>
-        /// <returns></returns>
-        public bool GetPinStatus(Pcf8563Pin pin)
+        public byte RawRead()
         {
-            var bit = GetPinBit(pin);
-            if ((inputPins & bit) == 0x00)
-                throw new NotSupportedException(string.Format(CultureInfo.InvariantCulture, "Cannot get value of input pin {0}", pin));
-
-            var status = connection.ReadByte();
-            return (status & bit) != 0x00;
+            return connection.ReadByte(); 
         }
 
-        /// <summary>
-        /// Toogles the specified pin.
-        /// </summary>
-        /// <param name="pin">The pin.</param>
-        public void Toogle(Pcf8563Pin pin)
+        public void WriteDateTime(DateTime ToSet)
         {
-            var bit = GetPinBit(pin);
-            if ((inputPins & bit) != 0x00)
-                throw new NotSupportedException(string.Format(CultureInfo.InvariantCulture, "Cannot set value of input pin {0}", pin));
-            
-            var status = currentStatus;
 
-            var bitEnabled = (status & bit) != 0x00;
-            var newBitEnabled = !bitEnabled;
+        }
 
-            var newStatus = (byte)(newBitEnabled
-                ? status | bit
-                : status & ~bit);
+        public void SetLinuxClockWithRtc()
+        {
 
-            connection.Write(newStatus);
-            currentStatus = newStatus;
         }
 
         #endregion
 
         #region Internal Helpers
 
-        internal void SetInputPin(Pcf8563Pin pin, bool isInput)
+        internal int bcd_to_int(int bcd)
         {
-            var bit = GetPinBit(pin);
-            inputPins = (byte) (isInput
-                ? inputPins | bit
-                : inputPins & ~bit);
+            int outN = bcd;
+            //// Decode a 2x4bit BCD to a integer.
+            //int out = 0; 
+            //for d in (bcd >> 4, bcd):
+            //    for p in (1, 2, 4 ,8):
+            //        if d & 1:
+            //            out += p
+            //        d >>= 1
+            //    out *= 10
+            //return out / 10; 
+            return outN; 
+        }
+
+        internal int int_to_bcd(int n)
+        {
+            int outBcd = n; 
+            //    """Encode a one or two digits number to the BCD.
+            //    """
+            //    bcd = 0
+            //    for i in (n // 10, n % 10):
+            //        for p in (8, 4, 2, 1):
+            //            if i >= p:
+            //                bcd += 1
+            //                i -= p
+            //            bcd <<= 1
+            //    return bcd >> 1 
+            return outBcd; 
         }
 
         #endregion
 
         #region Private Helpers
 
-        private static byte GetPinBit(Pcf8563Pin pin)
+        private enum Register
         {
-            return (byte) (int) pin;
+            REG_CONTROL_1 = 0x00,
+            REG_CONTROL_2 = 0x01,
+            REG_SECONDS = 0x02,
+            REG_MINUTES = 0x03,
+            REG_HOURS = 0x04,
+            REG_DAY = 0x06,
+            REG_DATE = 0x05,
+            REG_MONTH = 0x07,
+            REG_YEAR = 0x08,
         }
 
         #endregion
